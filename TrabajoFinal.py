1b672ec79d7d7b64e6446c6e00713eb649e3bea8
julio ponce <julio_09_12_99@hotmail.com>
2019-05-28 08:32:00 -0500

    entrada y pozo

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -1,23 +1,23 @@
+G=[]
+H=[]
+#Entrada
+x = int(input("ingrese el numero de jugadores: "))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while True:
+    y = input("ingrese su nombre: ")
+    G.append(y)
+    z= int(input("ingrese numero de cartillas: "))
+    if z <=3:
+        H.append(z)
+    else:
+        z = int(input("Ingrese numero de cartillas: "))
+    if len(G) == x:
+        break
 
 suma = 0 
 for fila in H:
     suma = suma + fila
 
 pozo = suma * 5
+
+print(pozo)
