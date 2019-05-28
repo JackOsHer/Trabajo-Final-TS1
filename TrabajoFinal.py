be80b2a47cb56d82b973321eda20cd0fd1fef63d
julio ponce <julio_09_12_99@hotmail.com>
2019-05-28 07:57:26 -0500

    primera parte

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -1,1 +1,13 @@
+G=[]
+H=[]
 #Entrada
+x = int(input("ingrese el numero de jugadores"))
+
+while True:
+    y = input("ingrese su nombre")
+    G.append(y)
+    z=int(input("ingrese numero de cartillas"))
+    H.append(z)
+    if len(G) == x and len(H) == z:
+        break
+
