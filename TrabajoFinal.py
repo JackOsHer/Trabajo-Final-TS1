da424b055e0f5102ffdf7b23382ba2cc59847789
julio ponce <julio_09_12_99@hotmail.com>
2019-05-28 08:12:34 -0500

    pozo

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -1,15 +1,20 @@
-G=[]
-H=[]
-#Entrada
-x = int(input("ingrese el numero de jugadores"))
 
-while True:
-    y = input("ingrese su nombre")
-    G.append(y)
-    z=int(input("ingrese numero de cartillas"))
-    H.append(z)
-    if len(G) == x and len(H) == z:
-        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 suma = 0 
 for fila in H:
