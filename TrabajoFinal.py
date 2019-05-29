8758b8df5f753cfdedc658afe8cc050198bcf27f
Seleny <abi-kgb@hotmail.com>
2019-05-29 07:46:04 -0500

    ultima entrada

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -6,12 +6,11 @@ x = int(input("ingrese el numero de jugadores: "))
 while True:
     y = input("ingrese su nombre: ")
     G.append(y)
-    z= int(input("ingrese numero de cartillas: "))
-    if z <=3:
-        H.append(z)
-    else:
-        z = int(input("Ingrese numero de cartillas: "))
-        H.append(z)
+    while True:
+        z= int(input("ingrese numero de cartillas: "))
+        if z <= 3:
+            H.append(z)
+            break
     if len(G) == x:
         break
 
